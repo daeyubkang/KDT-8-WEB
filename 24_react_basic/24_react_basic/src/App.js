32ea7a0eaf3aae5813d28278fecb2350661bc3fd
daeyubkang <yub102447@gmail.com>
2023-09-18 16:01:03 +0900

    react setting

--- 24_react_basic/24_react_basic/src/App.js
+++ 24_react_basic/24_react_basic/src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const users = [
+    { id: 1, name: "John", age: 25, vip: true },
+    { id: 2, name: "Jane", age: 19, vip: false },
+    { id: 3, name: "Alice", age: 30, vip: true },
+    { id: 4, name: "Bob", age: 18, vip: false },
+    { id: 5, name: "Charlie", age: 35, vip: true },
+  ];
+  let item2 = users.filter((txt) => {
+    return txt.age > 20;
+  });
+  console.log(item2);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {users.map((txt, index) => {
+        if (txt.vip) {
+          return <h1 key={txt.id}>{txt.name}</h1>;
+        }
+      })}
+      <br />
+      <br />
+
+      {item2.map((value, index) => {
+        return <h1 key={value.id}>{value.name}</h1>;
+      })}
+
+      {/* {item2.map((value, index) => {
+        return <h1>{value}</h1>;
+      })} */}
+
+      {/* <div className="test">{title}</div>
+      <br />
+      <div className="input">
+        아이디: <input type="text" />
+        <br />
+        <br />
+        비밀번호: <input type="text" />
+      </div>
+
+      <br />
+
+      <div style={{ display: "flex" }}>
+        <div className="red"></div>
+        <div className="orange"></div>
+        <div className="yellow"></div>
+        <div className="green"></div>
+        <div className="blue"></div>
+        <div className="navy"></div>
+        <div className="purple"></div>
+      </div> */}
+    </>
   );
 }
 
